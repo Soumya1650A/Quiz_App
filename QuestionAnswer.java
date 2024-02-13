203cf6570539fa969df96b41864df03016ad56cd
Soumya Ranjan Patnaik <155571977+Soumya1650A@users.noreply.github.com>
2024-02-13 09:40:12 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,23 @@
+
+public class QuestionAnswer {
+    public static String question []={
+            "Where was the India's first Museum opened?",
+            "What does RAD stands for?",
+            "Who is the father of Software Engineering?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"Delhi" , "Mumbai" , "Rajasthan" , "Hyderabad"},
+            {"Rapid Application Development" , "Rapid Application Document" , "Relative Application Development" , "None of the above"},
+            {"Margaret Hamilton" , "Watt S. Humphrey" , "Boris Beizer" , "Alan Turing"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "Mumbai" ,
+            "Rapid Application Development",
+            "Watt S. Humphrey",
+            "Elon Musk",
+            "All of the above"};
+}
